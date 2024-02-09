79c99d155286c36b7c6463b4d210f48a93dc3d37
SERGSERR <59841276+SERGSERR@users.noreply.github.com>
2024-02-09 10:49:44 -0500

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Data Science Tools and Ecosystem</h1>
+# 
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# __Objectives:__
+# * Format bold text
+# * Insert Markdown cell
+# * Insert code Cell
+# * format tables
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 1. R 
+# 2. Scala.
+# 3. Julia.
+# 4.  JavaScript.
+# 5.  Java.
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. TensorFlow
+# 2. NumPy
+# 3. SciPy 
+# 4. Pandas
+# 5. Matplotlib 
+# 6. Keras
+# 7. SciKit-Learn
+#     
+
+# |Data Science Tool|
+# |---|
+# |Jupyter lab|
+# |Anaconda|
+# |Spyder|
+# 
+
+# <h3>Below are a few examples of evaluating arithmetic expressions in Python<h3/>
+# $$
+#   \int_0^\infty \frac{x^3}{e^x-1}\,dx = \frac{\pi^4}{15}
+# $$
+# <p>
+# <p>
+# $$
+# a_1=b_1+c_1\\
+# $$
+# 
+#     
+
+# In[12]:
+
+
+print ('This a simple arithmetic expression to mutiply then add integer')
+(3*4)+5
+
+
+# In[13]:
+
+
+print ('This will convert 200 minutes to hours by diving by 60')
+200/60
+
+
+# <h2>Author</h2>
+# Sergio Serrano Gómez
+
+# In[ ]:
+
+
+
+
